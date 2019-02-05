75acc89f00cd03a9daf01ea20dae4e554b4e0573
Naga Sandeep Ramachandruni <sandeep@heallo.ai>
2019-02-05 22:20:25 +0530

    changed variable names

--- train_balanced_class.sh
+++ train_balanced_class.sh
@@ -12,13 +12,14 @@ python train_inceptionv3.py $dataset $exp $meanfile 299 $classes $max_epoch
 
 ########## TEsting NEtwork #######################
 max_size=320
-for i in `seq 0 $max_epoch`
+min_size=240
+for epoch in `seq 0 $max_epoch`
 do
-   for j in `seq 240 20 $max_size`	
+   for size in `seq $min_size 20 $max_size`	
    do	   
        f="_checkpoint.pth.tar"
-       echo "epoch: ", $i, "resize_size: ", $j 
-       python test_inceptionv3.py $dataset $exp/$i$f $meanfile 299 $j $classes out_$exp
+       echo "epoch: ", $epoch, "resize_size: ", $size
+       python test_inceptionv3.py $dataset $exp/$epoch$f $meanfile 299 $size $classes out_$exp
    done    
 done
 
